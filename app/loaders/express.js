3a06079b7b0e69144318a5e7063bd4c5df101875
Jiwon <jeewonre@naver.com>
2021-09-23 11:31:10 +0900

    aws-xray 연동

--- app/loaders/express.js
+++ app/loaders/express.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
 import compression from 'compression';
+import AWSXRay from 'aws-xray-sdk;';
 
 // custom utils And middlewares
 import logger from '../libs/logger/index';
@@ -32,6 +33,7 @@ export default async app => {
   app.use(compression());
   app.use(logger.dev);
 
+  app.use(AWSXRay.express.openSegment('tuningApp'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
@@ -51,6 +53,7 @@ export default async app => {
   // custom Error controllers
   app.use(pageNotFoundError);
   app.use(respondInternalError);
+  app.use(AWSXRay.express.closeSegment());
 
   return app;
 };
